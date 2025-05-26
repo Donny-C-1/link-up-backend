646c52e804b206bfc9a5258b1d33b571cf08dfc7
Donny C <99075588+Donny-C-1@users.noreply.github.com>
2025-05-26 01:44:28 +0100

    Latest update (#4)
    * Add GitHub Actions workflows for testing, PR creation, and production deployment (#1)
    
    * Dev (#3)
    
    * Add GitHub Actions workflows for testing, PR creation, and production deployment
    
    * Implement WebSocket server with user registration and message broadcasting

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 import "dotenv/config";
 import express from "express";
 import router from "./routers/indexRouter.js";
+import http from "http";
+import { Server as WebSocketServer } from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
+const io = new WebSocketServer(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+const users = new Map();
 
 app.use(router);
 
-app.listen(process.env.PORT, () => {
+io.on("connection", socket => {
+    console.log(`A user is connected. Socket Id: ${socket.id}`);
+
+    socket.on("register_user", username => {
+        users.set(socket.id, username);
+        io.emit("users_update", Array.from(users.values()));
+    });
+
+    socket.on("send_message", data => {
+        io.emit("receive_message", data); //broadcast to all users
+    });
+
+    socket.on("disconnect", () => {
+        users.delete(socket.id);
+        io.emit("users_update", Array.from(users.values()));
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+
+server.listen(process.env.PORT, () => {
     console.log(`Server listening on PORT ${process.env.PORT}`);
 });
 
